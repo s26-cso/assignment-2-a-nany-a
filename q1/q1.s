2654003c76178bc51b7d54725d7da8c139fb52a1
Ananya <ananya1000b@gmail.com>
2026-04-08 21:14:30 +0530

    1st q done

---
+++ q1/q1.s
@@ -0,0 +1,114 @@
+# VAL- 0
+# LEFT- 4
+# RIGHT- 8
+
+.global make_node
+make_node:
+
+addi sp, sp, -16
+sw a0, 0(sp) # to store the input val
+sw ra, 4(sp) 
+li a0, 12  # this is the size of the struct
+call malloc 
+addi t0, a0, 0 # t0 contains add of allocated memory
+lw t5, 0(sp) # val is now loaded in t5
+sw t5, 0(t0) # val stored
+addi t4, x0, 0
+sw t4, 4(t0) # left stored
+sw t4, 8(t0) # right stored
+
+addi a0, t0, 0
+lw ra, 4(sp)
+addi sp, sp, 16
+ret
+
+
+.global insert
+insert:
+addi sp, sp, -16
+sw a0, 0(sp) # to store root
+sw a1, 4(sp) # to store val
+sw ra, 8(sp) 
+beq a0, x0, insertNode
+lw t1, 0(a0) # t1 has val of root node
+bge a1, t1, rightchild
+blt a1, t1, leftchild
+
+rightchild:
+lw t2, 8(a0) # t2 now has the add of right child
+addi a0, t2, 0 # now a0 has add of right child
+call insert # use call as i need to return here after insert is done so I can join the subtree to the root
+lw t3, 0(sp)
+sw a0, 8(t3) # joining the node to the right subtree of root
+lw a0, 0(sp)
+j exit
+
+leftchild:
+lw t2, 4(a0) # t2 now has the add of left child
+addi a0, t2, 0 # now a0 has add of left child
+call insert
+lw t3, 0(sp)
+sw a0, 4(t3) # joining the node to the left subtree of root
+lw a0, 0(sp)
+j exit
+
+insertNode:
+addi a0, a1, 0
+call make_node
+
+exit:
+lw ra, 8(sp)
+addi sp, sp, 16
+ret
+
+
+.global get
+get:
+addi sp, sp, -16
+sw a0, 0(sp) # to store add of root
+sw ra, 8(sp) 
+beq a0, x0, exit
+lw t0, 0(a0) # t0 now has value of root
+beq t0, a1, exit
+blt a1, t0, leftchild
+bgt a1, t0, rightchild
+
+leftchild:
+lw a0, 4(a0) # 4(a0) has the value that is the address of leftchild
+call get
+j exit
+
+rightchild:
+lw a0, 8(a0)
+call get
+j exit
+
+exit:
+lw ra, 8(sp)
+addi sp, sp, 16
+ret
+
+.global getAtMost
+getAtMost:
+addi t5, x0, -1 # t5 will store the ans
+loop:
+# a0 to store val
+# a1 to store add of root
+
+beq a1, x0, exit
+lw t0, 0(a1) # t0 now has the value of root
+blt a0, t0, leftchild
+bge a0, t0, rightchild # t0 is a valid choice for ans
+
+leftchild:
+lw a1, 4(a1) # 4(a1) has the value that is the address of leftchild
+j loop
+
+rightchild:
+addi t5, t0, 0
+lw a1, 8(a1)
+j loop
+
+exit:
+addi a0, t5, 0
+ret
